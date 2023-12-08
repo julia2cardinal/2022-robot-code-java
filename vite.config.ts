e041c54b19785237dcec4b530a66198b39da9a1f
Julia Gao <80399307+julia2cardinal@users.noreply.github.com>
2023-12-07 17:48:25 -0700

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+// src/main/java/com/example/backend/Application.java
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
